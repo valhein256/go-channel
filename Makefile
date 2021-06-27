02dec8bfcddc0fcdc2db05fb7483ad7016a4c561
valhein <valhein256@gmail.com>
2021-06-27 15:36:26 +0800

    Init

---
+++ Makefile
@@ -0,0 +1,31 @@
+SERVICE := go
+STAGE := stg
+PLATFORM := local
+WORKSPACE := /opt/go
+APP := ${WORKSPACE}/channel.go
+
+.PHONY: build run devenv update clean
+
+build: clean
+	@echo "Start building..."
+	@DOCKER_BUILDKIT=1 docker build --pull . \
+		--platform ${PLATFORM} \
+		--build-arg TAG=${STAGE} \
+		--target release --output bin/
+
+run:
+	@docker run \
+		-v ${PWD}:${WORKSPACE} \
+		--rm -it golang:1-buster \
+		go run ${APP}
+
+devenv:
+	@docker run \
+		-v ${PWD}:/opt/go \
+		--rm -it golang:1-buster /bin/bash
+
+update:
+	@go mod tidy
+
+clean:
+	@rm -rf bin
---
+++ channel.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"math/rand"
+)
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+func (n *Node) insert(value int) {
+	if value <= n.value {
+		if n.left == nil {
+			n.left = &Node{value: value}
+		} else {
+			n.left.insert(value)
+		}
+	} else {
+		if n.right == nil {
+			n.right = &Node{value: value}
+		} else {
+			n.right.insert(value)
+		}
+	}
+}
+
+type Tree struct {
+	node *Node
+}
+
+func (t *Tree) insert(value int) {
+	if t.node == nil {
+		println("insert first item to tree", value)
+		t.node = &Node{value: value}
+	} else {
+		t.node.insert(value)
+	}
+}
+
+func (n *Node) search(value int) int {
+	if n == nil {
+		return -1
+	}
+
+	if value == n.value {
+		return n.value
+	}
+
+	if value < n.value {
+		return n.left.search(value)
+	} else {
+		return n.right.search(value)
+	}
+}
+
+func printTree(n *Node, sort_type string) {
+	if n == nil {
+		return
+	}
+	switch sort_type {
+	case "ascending":
+		printTree(n.left, sort_type)
+		println(n.value)
+		printTree(n.right, sort_type)
+		break
+	case "descending":
+		printTree(n.right, sort_type)
+		println(n.value)
+		printTree(n.left, sort_type)
+		break
+	default:
+		println(n.value)
+		printTree(n.left, sort_type)
+		printTree(n.right, sort_type)
+		break
+	}
+}
+
+func process_generate_tree(ch chan *Tree) {
+	t := &Tree{}
+	for i := 0; i < 10; i++ {
+		n := rand.Intn(100)
+		t.insert(n)
+	}
+	ch <- t
+}
+
+func process_print_tree(ch chan *Tree) {
+	t := &Tree{}
+	for i := 0; i < 10; i++ {
+		n := rand.Intn(100)
+		t.insert(n)
+	}
+	ch <- t
+}
+
+func main() {
+	ch := make(chan *Tree)
+	go process_generate_tree(ch)
+	t := <-ch
+
+	println("default")
+	printTree(t.node, "ascending")
+	println()
+}
